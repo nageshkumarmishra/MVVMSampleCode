8f716bc437acbee069f18e6a0eacf04dbcc094c1
Nagesh Kumar Mishra <nageshkumarmishra@gmail.com>
2021-06-01 19:28:28 +0530

    - Lunch screen design changes - Cards design using collection view - App icons added with default avatar

--- TradingCard/CollectionViewCell/CollectionViewCell.swift
+++ TradingCard/CollectionViewCell/CollectionViewCell.swift
@@ -5,4 +5,27 @@
 //  Created by Nagesh Kumar Mishra on 01/06/21.
 //
 
-import Foundation
+import UIKit
+
+class DataCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dataView: UIView!
+    @IBOutlet weak var expireDate: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var cardNumber: UILabel!
+    @IBOutlet weak var cardType: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.dataView.layer.cornerRadius = 15.0
+    }
+    
+    var card : Cards? {
+        didSet {
+            self.cardNumber.text = card?.cardNumber
+            self.userName.text = card?.userName
+            self.expireDate.text = card?.expiryDate
+            self.cardType.text = card?.cardType
+        }
+    }
+}
