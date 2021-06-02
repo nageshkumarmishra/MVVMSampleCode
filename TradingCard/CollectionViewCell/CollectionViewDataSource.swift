fe24e880157cc72b953e6d772018e9cfb7cac4d1
Nagesh Kumar Mishra <nageshkumarmishra@gmail.com>
2021-06-02 07:42:26 +0530

    - Collection view data source - Data binding to View elements

---
+++ TradingCard/CollectionViewCell/CollectionViewDataSource.swift
@@ -0,0 +1,32 @@
+//
+//  CollectionViewDataSource.swift
+//  TradingCard
+//
+//  Created by Nagesh Kumar Mishra on 01/06/21.
+//
+
+import UIKit
+
+class CollectionViewDataSource<CELL : UICollectionViewCell,T> : NSObject, UICollectionViewDataSource {
+    
+    private var cellIdentifier : String!
+    private var items : [T]?
+    var configureCell : (CELL, T?) -> () = {_,_ in }
+    
+    init(cellIdentifier : String, items : [T]?, configureCell : @escaping (CELL, T?) -> ()) {
+        self.cellIdentifier = cellIdentifier
+        self.items =  items
+        self.configureCell = configureCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! CELL
+        let item = self.items?[indexPath.row]
+        self.configureCell(cell, item)
+        return cell
+    }
+}
--- TradingCard/Controllers/ViewController.swift
+++ TradingCard/Controllers/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UICollectionViewController {
-
+    
+    private var cardsViewModel : CardsViewModel!
+    private var dataSource : CollectionViewDataSource<DataCollectionViewCell,Cards>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        callToViewModelForUIUpdate()
     }
+    
+    func callToViewModelForUIUpdate(){
+        self.cardsViewModel =  CardsViewModel()       
+        self.cardsViewModel.bindViewModelToController = {
+            self.updateDataSource()
+        }
+    }
+    
+    func updateDataSource(){
+        
+        self.dataSource = CollectionViewDataSource(cellIdentifier: "reuseIdentifier", items: self.cardsViewModel.dataFeed?.cards, configureCell: { (cell, data) in
+            cell.card = data
+        })
+        
+        DispatchQueue.main.async {
+            self.collectionView.dataSource = self.dataSource
+            self.collectionView.delegate = self
+            self.collectionView.reloadData()
+        }
+    }
+}
 
-
+extension ViewController : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 4;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 141.0)
+    }
+    
 }
 
